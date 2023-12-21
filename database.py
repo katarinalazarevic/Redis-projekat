2c1e396aa6e85e60e2ecf7f97caee2bee8002c7c
PetarMancic <petarmancic@elfak.rs>
2023-12-21 17:55:12 +0100

    Povezivanje redisa

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
+import redis
+from redis import Redis
 
 engine = create_engine('postgresql://postgres:petarbaze@localhost/Projekat1') #konekcija sa bazom
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -8,6 +10,8 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
+
+redis_client = Redis(host='localhost', port=6379, decode_responses=True)
 
 def init_db():
     # import all modules here that might define models so that
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from flask import Flask
-from app.models import Probas
-from database import init_db, db_session
-
+#from app.models import Probas
+from database import init_db, db_session, redis_client
 app1 = Flask(__name__)
 init_db()
 
@@ -10,8 +9,12 @@ def shutdown_session(exception=None):
     db_session.remove()
 
 def main():
-    u = Probas('admin2', 'admin@localhost2')
-    db_session.add(u)
-    db_session.commit()
+   # u = Probas('admin2', 'admin@localhost2')
+   # db_session.add(u)
+   # db_session.commit()
+
+    redis_client.set('kljuc1', 'vrednost1')
+    value1 = redis_client.get('kljuc1')
+    print(value1)
 if __name__ == '__main__':
     main()
